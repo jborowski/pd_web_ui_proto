abecf8a7a720f486542135bec1b20613544ed42c
Jędrzej <jedrzej@MacBook-Pro-Jedrzej.local>
2017-01-09 23:47:11 +0100

    additional logic and labels

--- ui.js
+++ ui.js
@@ -1,7 +1,7 @@
-var loop1Reversed = false;
-var loop2Reversed = false;
-var loop3Reversed = false;
-var loop4Reversed = false;
+var loop11Reversed = false;
+var loop12Reversed = false;
+var loop21Reversed = false;
+var loop22Reversed = false;
 
 nx.onload = function() {
 
@@ -9,118 +9,184 @@ nx.onload = function() {
 
     button1.mode = "impulse"
     button1.on('*', function(data) {
-        if (!loop1.paused) {
-            loop1.pause();
-            loop1.currentTime = 0;
+        if (!loop11.paused) {
+            loop11.pause();
+            loop11.currentTime = 0;
             resetSliders();
         } else {
             resetSliders();
-            loop1.play();
+            loop11.play();
         }
     });
 
-    loop1.onended = function() {
-        if (dial11.val.value > 0.5 && dial11.val.value < 0.75 ){
-            loop2Reversed = false;
-            loop2.play();
-        } else if (dial11.val.value > 0.75 ) {
-            loop4Reversed = false;
-            loop4.play();
-        } else if (dial11.val.value < 0.5) {
-            loop1Reversed = true;
-            loop1_rev.play();
+    loop11.onended = function() {
+        if (dial11.label == "fwd"){
+            loop12Reversed = false;
+            loop12.play();
+        } else if (dial11.label == "down" ) {
+            loop22Reversed = false;
+            loop22.play();
+        } else if (dial11.label == "back") {
+            loop11Reversed = true;
+            loop11_rev.play();
         }
     };
 
-    loop1_rev.onended = function() {
-        loop1Reversed = false;
-        loop1.play();
+    loop11_rev.onended = function() {
+        loop11Reversed = false;
+        loop11.play();
     }
 
-    loop2.onended = function() {
-        if (dial12.val.value < 0.5) {
-            loop2Reversed = true;
-            loop2_rev.play();
+    loop12.onended = function() {
+        if (dial12.label == "back") {
+            loop12Reversed = true;
+            loop12_rev.play();
         }
     }
 
-    loop2_rev.onended = function() {
-        if (dial11.val.value > 0.5) {
-            loop2Reversed = false;
-            loop2.play();
-        } else if (dial11.val.value < 0.5) {
-            loop1Reversed = true;
-            loop1_rev.play();
+    loop12_rev.onended = function() {
+        if (dial11.label == "fwd") {
+            loop12Reversed = false;
+            loop12.play();
+        } else if (dial11.label == "back") {
+            loop11Reversed = true;
+            loop11_rev.play();
+        } else if (dial11.label == "down") {
+            loop21Reversed = true;
+            loop21_rev.play();
         }
     }
 
-    loop3.onended = function() {
-        if (dial21.val.value > 0.25 && dial21.val.value < 0.75 ){
-            loop2Reversed = false;
-            loop2.play();
-        } else if (dial11.val.value > 0.75 ) {
-            loop4Reversed = false;
-            loop4.play();
-        } else if (dial11.val.value < 0.25) {
-            loop3Reversed = true;
-            loop3_rev.play();
+    loop21.onended = function() {
+        if (dial21.label == "up" ){
+            loop12Reversed = false;
+            loop12.play();
+        } else if (dial21.label == "fwd" ) {
+            loop22Reversed = false;
+            loop22.play();
+        } else if (dial11.label == "back") {
+            loop21Reversed = true;
+            loop21_rev.play();
         }
     };
 
-    loop3_rev.onended = function() {
-        loop3Reversed = false;
-        loop3.play();
+    loop21_rev.onended = function() {
+        loop21Reversed = false;
+        loop21.play();
     }
 
-    loop4.onended = function() {
-        if (dial22.val.value < 0.5) {
-            loop4Reversed = true;
-            loop4_rev.play();
+    loop22.onended = function() {
+        if (dial22.label == "back") {
+            loop22Reversed = true;
+            loop22_rev.play();
         }
     }
 
-    loop4_rev.onended = function() {
-        if (dial21.val.value > 0.5) {
-            loop4.Reversed = false;
-            loop4.play();
-        } else if (dial21.val.value < 0.5) {
-            loop3Reversed = true;
-            loop3_rev.play();
+    loop22_rev.onended = function() {
+        if (dial21.label == "fwd") {
+            loop22.Reversed = false;
+            loop22.play();
+        } else if (dial21.label == "back") {
+            loop21Reversed = true;
+            loop21_rev.play();
+        } else if (dial21.label == "up") {
+            loop11Reversed = true;
+            loop11_rev.play();
         }
     }
 
-    var updateTimer = setInterval(update, 1);
+    var updateTimer = setInterval(update, 33);
 }
 
 function update() {
 
-    if (loop1Reversed) {
-        slider11.val.value = 1 - loop1_rev.currentTime / loop1_rev.duration;
+    if (loop11Reversed) {
+        slider11.val.value = 1 - loop11_rev.currentTime / loop11_rev.duration;
     } else {
-        slider11.val.value = loop1.currentTime / loop1.duration;
+        slider11.val.value = loop11.currentTime / loop11.duration;
     }
     slider11.draw();
 
-    if (loop2Reversed) {
-        slider12.val.value = 1 - loop2_rev.currentTime / loop2_rev.duration;
+    if (loop12Reversed) {
+        slider12.val.value = 1 - loop12_rev.currentTime / loop12_rev.duration;
     } else {
-        slider12.val.value = loop2.currentTime / loop2.duration;
+        slider12.val.value = loop12.currentTime / loop12.duration;
     }
     slider12.draw();
 
-    if (loop3Reversed) {
-        slider21.val.value = 1 - loop3_rev.currentTime / loop3_rev.duration;
+    if (loop21Reversed) {
+        slider21.val.value = 1 - loop21_rev.currentTime / loop21_rev.duration;
     } else {
-        slider21.val.value = loop3.currentTime / loop3.duration;
+        slider21.val.value = loop21.currentTime / loop21.duration;
     }
     slider21.draw();
 
-    if (loop4Reversed) {
-        slider22.val.value = 1 - loop4_rev.currentTime / loop4_rev.duration;
+    if (loop22Reversed) {
+        slider22.val.value = 1 - loop22_rev.currentTime / loop22_rev.duration;
     } else {
-        slider22.val.value = loop4.currentTime / loop4.duration;
+        slider22.val.value = loop22.currentTime / loop22.duration;
     }
     slider22.draw();
+
+    if (dial11.val.value >= 0.12 && dial11.val.value < 0.37) {
+        dial11.label = "back";
+    } else if (dial11.val.value >= 0.37 && dial11.val.value < 0.62) {
+        dial11.label = "up";
+    } else if (dial11.val.value >= 0.62 && dial11.val.value < 0.87) {
+        dial11.label = "fwd";
+    } else {
+        dial11.label = "down";
+    }
+
+    if (dial12.val.value >= 0.12 && dial12.val.value < 0.37) {
+        dial12.label = "back";
+    } else if (dial12.val.value >= 0.37 && dial12.val.value < 0.62) {
+        dial12.label = "up";
+    } else if (dial12.val.value >= 0.62 && dial12.val.value < 0.87) {
+        dial12.label = "fwd";
+    } else {
+        dial12.label = "down";
+    }
+
+    if (dial13.val.value >= 0.13 && dial13.val.value < 0.37) {
+        dial13.label = "back";
+    } else if (dial13.val.value >= 0.37 && dial13.val.value < 0.62) {
+        dial13.label = "up";
+    } else if (dial13.val.value >= 0.62 && dial13.val.value < 0.87) {
+        dial13.label = "fwd";
+    } else {
+        dial13.label = "down";
+    }
+
+    if (dial21.val.value >= 0.12 && dial21.val.value < 0.37) {
+        dial21.label = "back";
+    } else if (dial21.val.value >= 0.37 && dial21.val.value < 0.62) {
+        dial21.label = "up";
+    } else if (dial21.val.value >= 0.62 && dial21.val.value < 0.87) {
+        dial21.label = "fwd";
+    } else {
+        dial21.label = "down";
+    }
+
+    if (dial22.val.value >= 0.12 && dial22.val.value < 0.37) {
+        dial22.label = "back";
+    } else if (dial22.val.value >= 0.37 && dial22.val.value < 0.62) {
+        dial22.label = "up";
+    } else if (dial22.val.value >= 0.62 && dial22.val.value < 0.87) {
+        dial22.label = "fwd";
+    } else {
+        dial22.label = "down";
+    }
+
+    if (dial23.val.value >= 0.12 && dial23.val.value < 0.37) {
+        dial23.label = "back";
+    } else if (dial23.val.value >= 0.37 && dial23.val.value < 0.62) {
+        dial23.label = "up";
+    } else if (dial23.val.value >= 0.62 && dial23.val.value < 0.87) {
+        dial23.label = "fwd";
+    } else {
+        dial23.label = "down";
+    }
 
 }
 
@@ -129,8 +195,12 @@ function resetSliders() {
     slider11.draw();
     slider12.val.value = 0;
     slider12.draw();
+    slider13.val.value = 0;
+    slider13.draw();
     slider21.val.value = 0;
     slider21.draw();
     slider22.val.value = 0;
     slider22.draw();
+    slider23.val.value = 0;
+    slider23.draw();
 }
